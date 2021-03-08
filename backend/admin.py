031ce2f36ce1cd2d9efe874475c4fe5882080b87
kevin.laipe <kevin.laipe@he-arc.ch>
2021-03-08 17:03:12 +0100

    Add fully fledged data to Fridge model

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Fridge
+from .models import Picture
 
 admin.site.register(Fridge)
+admin.site.register(Picture)
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 class Fridge(models.Model):
+    def menu_list_default():
+        return {"menu": { "name": "", "allergens": ""}}
+    
     name = models.CharField(max_length=250)
-    #TODO: Add photos, menu list, My Maps' noticeboard, manager's noticeboard
+    my_maps_description = models.TextField(default="My Maps description")
+    manager_description = models.TextField(default="Manager description")
+    menu_list = models.JSONField(default=menu_list_default)
 
     def __str__(self):
         return self.name
+
+
+class Picture(models.Model):
+    image = models.ImageField()
+    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE)
