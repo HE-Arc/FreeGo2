3048183fc08ff06ae13e6b6ff776b68f487d3c9f
kevin.laipe <kevin.laipe@he-arc.ch>
2021-03-10 13:25:23 +0100

    Add vuetify

--- frontend/vueapp/src/main.js
+++ frontend/vueapp/src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import router from './routes.js'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
 
 router.beforeEach((to, from, next) => {
