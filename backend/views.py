3b39ee962d2b92d21c35b9787cd3872f6eabb825
kevin.laipe <kevin.laipe@he-arc.ch>
2021-03-10 13:22:18 +0100

    Remove no more useful import

--- backend/views.py
+++ backend/views.py
@@ -5,7 +5,6 @@ from .serializers import FridgeSerializer, PictureSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
-from wsgiref.util import FileWrapper
 
 class FridgeViewSet(viewsets.ModelViewSet):
     renderer_classes = [JSONRenderer,]
